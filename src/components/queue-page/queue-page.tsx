b56becb485c3e4cc4c248fd2e0dfa700a4e4f962
Aleksandr Petrov <petrov.aleksr@gmail.com>
2023-07-30 03:14:04 +0500

    feat: реализована функциональность QueuePage

--- src/components/queue-page/queue-page.tsx
+++ src/components/queue-page/queue-page.tsx
@@ -1,87 +1,173 @@
-import React, { useState, ChangeEvent } from 'react'
-import { SolutionLayout } from '../ui/solution-layout/solution-layout'
 import styles from './queue-page.module.css'
+import React, { useState } from 'react'
 import { useForm } from '../../hooks/useForm'
+import { SolutionLayout } from '../ui/solution-layout/solution-layout'
 import { Input } from '../ui/input/input'
 import Button from '../ui/button/button'
 import Circle from '../ui/circle/circle'
 import { ElementStates } from '../../types/element-states'
+import { Queue } from '../../utils/queue'
 import { delay } from '../../utils/delay'
+import { ButtonsHookState } from '../../types/types'
+
 
 export const QueuePage: React.FC = () => {
 
-  const { values, errors, isFormValid, handleChange, checkIsFormValid } = useForm( { queueInput: '' } )
+  const DELAY_TIME = 500
+  const QUEUE_LENGTH = 7
 
-  const [ isAnimating, setIsAnimating ] = useState( true )
+  const validateConfig = {
+    queueInput: {
+      pattern: /^[a-zA-Zа-яА-Я0-9]{0,4}$/,
+      checkIsEmptyValue: true,
+      defaultErrorMessage: 'Допустимы только буквы и цифры!!!',
+    }
+  }
+
+  const {
+    values,
+    errors,
+    isFormValid,
+    handleChange,
+    checkIsFormValid,
+    resetField
+  } = useForm( { queueInput: '' }, validateConfig )
+
+  const [ queue, setQueue ] = useState( new Queue<string>( QUEUE_LENGTH ) )
+  const [ highlightedIndex, setHighlightedIndex ] = useState<number | null>( null )
+  const [ isAllHighlighted, setIsAllHighlighted ] = useState( false )
+  const [ prevData, setPrevData ] = useState( { head: 0, tail: 0 } ) 
   const [ isFormDisabled, setIsFormDisabled ] = useState( false )
-  const [ symbolsArr, setSymbolsArr ] = useState<number[]>( [] )
+  const [ buttonsState, setButtonsState ] = useState<ButtonsHookState>( {
+    add: { isLoading: false },
+    remove: { isLoading: false },
+    clear: { isLoading: false }
+  } )
 
-  async function animate () {
-    console.log( 'ok' )
+  function blockForm ( buttonName: string ) {
+    setIsFormDisabled( true )
+    setButtonsState( ( { ...buttonsState, [ buttonName ]: { isLoading: true } } ) )
+  }
+
+  function activateForm ( buttonName: string ) {
+    setIsFormDisabled( false )
+    setButtonsState( ( { ...buttonsState, [ buttonName ]: { isLoading: false } } ) )
+  }
+
+  // Эти константы нужны, чтобы не допустить зацикливание
+  const isMaxTailIndex = prevData.tail === QUEUE_LENGTH - 1
+  const isMaxHeadIndex = prevData.head === QUEUE_LENGTH - 1
+
+  const handleAdd = async () => {
+    if ( !checkIsFormValid() || isMaxTailIndex ) { return }
+    setPrevData( { ...prevData, tail: queue.tail } )
+    blockForm( 'add' )
+    setHighlightedIndex( queue.tail )
+    await delay( DELAY_TIME )
+    queue.enqueue( values.queueInput )
+    resetField( 'queueInput' )
+    setQueue( queue )
+    setHighlightedIndex( null )
+    activateForm( 'add' )
+  }
+
+  const handleRemove = async () => {
+    if ( isMaxHeadIndex ) { return }
+    setPrevData( { ...prevData, head: queue.head } )
+    blockForm( 'remove' )
+    setHighlightedIndex( queue.head )
+    await delay( DELAY_TIME )
+    queue.dequeue()
+    setQueue( queue )
+    setHighlightedIndex( null )
+    activateForm( 'remove' )
+  }
+
+  const handleClear = async () => {
+    if ( queue.isEmpty && !isMaxHeadIndex ) { return }
+    setPrevData( { head: 0, tail: 0 } )
+    blockForm( 'clear' )
+    setIsAllHighlighted( true )
+    await delay( DELAY_TIME )
+    queue.clear()
+    setQueue( queue )
+    setIsAllHighlighted( false )
+    activateForm( 'clear' )
   }
 
   const handleSubmit = ( e: React.FormEvent ) => {
     e.preventDefault()
-    setTimeout( () => animate(), 800 )
+    handleAdd()
   }
 
-  return <SolutionLayout title='Очередь'>
+  return (
+    <SolutionLayout title='Очередь'>
+      <form className={ styles.formWrapper } onSubmit={ handleSubmit }    >
+        <Input
+          name='queueInput'
+          placeholder='Введите значение'
+          isLimitText={ true }
+          limitText={ errors.queueInput ? errors.queueInput : 'Максимум — 4 символа' }
+          value={ values.queueInput }
+          maxLength={ 4 }
+          onChange={ handleChange }
+          disabled={ isFormDisabled || isMaxTailIndex || isMaxHeadIndex }
+        />
 
-    <form className={ styles.formWrapper } onSubmit={ handleSubmit }>
+        <Button
+          type='button'
+          linkedList='small'
+          extraClass={ styles.button_correct_medium }
+          text={ buttonsState.add.isLoading ? '' : 'Добавить' }
+          isLoader={ buttonsState.add.isLoading ? true : false }
+          disabled={ isFormDisabled && !buttonsState.add.isLoading || !isFormValid || isMaxTailIndex || isMaxHeadIndex }
+          onClick={ handleAdd }
+        />
 
-      <Input
-        placeholder='Введите значение'
-        isLimitText={ true }
-        limitText={ errors.queueInput ? errors.queueInput : 'Максимум — 4 символа' }
-        value={ values.queueInput }
-        name='queueInput'
-        onChange={ ( e: ChangeEvent<HTMLInputElement> ) => {
-          handleChange( e )
-        } }
-        onFocus={ () => {
-          setIsAnimating( false )
-          setSymbolsArr( [] )
-        } }
-        disabled={ isFormDisabled }
-      />
+        <Button
+          type='button'
+          linkedList='small'
+          extraClass={ `${ styles.button_correct_small } ${ styles.button_correct_mr }` }
+          text={ buttonsState.remove.isLoading ? '' : 'Удалить' }
+          isLoader={ buttonsState.remove.isLoading ? true : false }
+          disabled={ isFormDisabled && !buttonsState.remove.isLoading || queue.isEmpty || isMaxHeadIndex }
+          onClick={ handleRemove }
+        />
 
-      <Button
-        //isLoader={ isFormDisabled }
-        text={ isFormDisabled ? '' : 'Добавить' }
-        type='button'
-        linkedList='small'
-        extraClass={ styles.button_correct_medium }
-      //disabled={ !isFormValid }
-      />
+        <Button
+          type='button'
+          linkedList='small'
+          extraClass={ styles.button_correct_medium }
+          text={ buttonsState.clear.isLoading ? '' : 'Очистить' }
+          isLoader={ buttonsState.clear.isLoading ? true : false }
+          disabled={ isFormDisabled && !buttonsState.clear.isLoading || queue.isEmpty && !isMaxHeadIndex }
+          onClick={ handleClear }
+        />
 
-      <Button
-        //isLoader={ isFormDisabled }
-        text={ isFormDisabled ? '' : 'Удалить' }
-        type='button'
-        linkedList='small'
-        extraClass={ `${ styles.button_correct_small } ${ styles.button_correct_mr }` }
-      //disabled={ !isFormValid }
-      />
+      </form>
+      <div className={ styles.blockAnimate }>
+        { queue.elements.map( ( el, index ) => (
+          <Circle
+            key={ index }
+            index={ index }
+            tail={
+              !queue.isEmpty && index === ( queue.tail - 1 + queue.elements.length ) % queue.elements.length ? 'tail' : ''
+            }
+            head={ ( index === queue.head && !queue.isEmpty ) ||
+              ( queue.isEmpty && index !== 0 && index === queue.head ) ||
+              ( isMaxHeadIndex && index === QUEUE_LENGTH - 1 )
+              ? 'head'
+              : '' }
+            letter={ el }
+            state={
+              ( index === highlightedIndex || isAllHighlighted )
+                ? ElementStates.Changing
+                : ElementStates.Default
+            }
+          />
+        ) ) }
+      </div>
 
-      <Button
-        //isLoader={ isFormDisabled }
-        text={ isFormDisabled ? '' : 'Очистить' }
-        type='button'
-        linkedList='small'
-        extraClass={ styles.button_correct_medium }
-      //disabled={ !isFormValid }
-      />
-
-    </form>
-
-    <div className={ styles.blockAnimate }>
-      <Circle /* key={ index } */ index={ 0 } tail={ '' } head={ 'head' } letter={ '2' } state={ ElementStates.Default } />
-      <Circle /* key={ index } */ index={ 1 } tail={ '' } head={ '' } letter={ '5' } state={ ElementStates.Default } />
-      <Circle /* key={ index } */ index={ 2 } tail={ '' } head={ '' } letter={ '3' } state={ ElementStates.Default } />
-      <Circle /* key={ index } */ index={ 3 } tail={ '' } head={ '' } letter={ '' } state={ ElementStates.Changing } />
-      <Circle /* key={ index } */ index={ 4 } tail={ '' } head={ '' } letter={ '' } state={ ElementStates.Default } />
-      <Circle /* key={ index } */ index={ 5 } tail={ '' } head={ '' } letter={ '' } state={ ElementStates.Default } />
-      <Circle /* key={ index } */ index={ 6 } tail={ '' } head={ '' } letter={ '' } state={ ElementStates.Default } />
-    </div>
-  </SolutionLayout>
+    </SolutionLayout >
+  )
 }
---
+++ src/utils/queue.ts
@@ -0,0 +1,42 @@
+class Queue<T> {
+
+  private data: ( T | undefined )[]
+
+  constructor ( size: number ) {
+    this.data = Array( size ).fill( undefined )
+  }
+
+  public head: number = 0;
+  public tail: number = 0;
+
+  enqueue ( item: T ) {
+    this.data[ this.tail ] = item
+    this.tail = ( this.tail + 1 ) % this.data.length
+  }
+
+  dequeue (): T | undefined {
+    if ( this.isEmpty ) {
+      return undefined
+    }
+    const item = this.data[ this.head ]
+    this.data[ this.head ] = undefined
+    this.head = ( this.head + 1 ) % this.data.length
+    return item
+  }
+
+  clear () {
+    this.data.fill( undefined )
+    this.head = 0
+    this.tail = 0
+  }
+
+  get isEmpty (): boolean {
+    return this.head === this.tail && this.data[ this.head ] === undefined
+  }
+
+  get elements (): ( T | undefined )[] {
+    return this.data
+  }
+}
+
+export { Queue }
