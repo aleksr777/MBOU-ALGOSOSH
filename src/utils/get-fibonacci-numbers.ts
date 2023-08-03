2708a50a4e98a82d528d0af2979a77a21c0b9cb5
Aleksandr Petrov <petrov.aleksr@gmail.com>
2023-08-03 15:46:22 +0500

    fix: Подправил мемоизацию для компонента FibonacciPage

--- src/utils/get-fibonacci-numbers.ts
+++ src/utils/get-fibonacci-numbers.ts
@@ -1,21 +1,32 @@
 
 import { OnlyNumberObjType } from '../types/types'
 
+
 export function getFibonacciNumbers ( num: number ) {
+
   const memo: OnlyNumberObjType = {}
   const arr: number[] = []
-  function fillArrNumbers ( a: number, b: number, limit: number, memo: OnlyNumberObjType ): void {
+
+  function fillArrNumbers ( a: number, b: number, limit: number ): void {
+    const memoKey = `${ a }_${ b }_${ limit }`
+
+    if ( memo[ memoKey ] !== undefined ) {
+      arr.push( memo[ memoKey ] )
+      return
+    }
+
     arr.push( a )
+
     if ( limit === 0 ) {
       return
     }
-    const memoKey = `${ a }_${ b }_${ limit }`
-    if ( memo[ memoKey ] !== undefined ) {
-      return
-    }
-    fillArrNumbers( b, a + b, limit - 1, memo )
-    memo[ memoKey ] = arr[ arr.length - 1 ]
+
+    fillArrNumbers( b, a + b, limit - 1 )
+
+    memo[ memoKey ] = a
   }
-  fillArrNumbers( 1, 1, num, memo )
+
+  fillArrNumbers( 1, 1, num )
+
   return arr
 }
