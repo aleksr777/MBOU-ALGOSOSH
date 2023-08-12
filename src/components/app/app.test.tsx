96b090077d0af46c649e114a1e5e63a5af33b6d5
Aleksandr Petrov <petrov.aleksr@gmail.com>
2023-08-12 18:35:47 +0500

    feat: добавил проверку роутинга в компоненте App

--- src/components/app/app.test.tsx
+++ src/components/app/app.test.tsx
@@ -1,9 +1,89 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
-import App from './app'
+import { MemoryRouter } from 'react-router-dom'
+import { render } from '@testing-library/react'
+import { FibonacciPage } from '../fibonacci-page/fibonacci-page'
+import { ListPage } from '../list-page/list-page'
+import { MainPage } from '../main-page/main-page'
+import { QueuePage } from '../queue-page/queue-page'
+import { StringComponent } from '../string/string'
+import { SortingPage } from '../sorting-page/sorting-page'
+import { StackPage } from '../stack-page/stack-page'
 
-test( 'renders main page title', () => {
-  render( <App /> )
-  const titleElement = screen.getByText( /МБОУ АЛГОСОШ/i )
-  expect( titleElement ).toBeInTheDocument()
+describe( '<App />', () => {
+
+  test( 'correct routing to MainPage ', async () => {
+    const { container } = render(
+      <MemoryRouter initialEntries={ [ '/' ] }>
+        <MainPage />
+      </MemoryRouter>
+    )
+
+    const links = [ '/recursion', '/fibonacci', '/sorting', '/stack', '/queue', '/list' ]
+
+    links.forEach( link => {
+      const linkElement = container.querySelector( `a[href="${ link }"]` )
+      expect( linkElement ).toBeInTheDocument()
+    } )
+  } )
+
+  test( 'correct routing to StringComponent', async () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ [ '/recursion' ] }>
+        <StringComponent />
+      </MemoryRouter>
+    )
+    const title = getByText( /строка/i )
+    expect( title ).toBeInTheDocument()
+  } )
+
+  test( 'correct routing to FibonacciPage', async () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ [ '/fibonacci' ] }>
+        <FibonacciPage />
+      </MemoryRouter>
+    )
+    const title = getByText( /последовательность фибоначчи/i )
+    expect( title ).toBeInTheDocument()
+  } )
+
+  test( 'correct routing to SortingPage', async () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ [ '/sorting' ] }>
+        <SortingPage />
+      </MemoryRouter>
+    )
+    const title = getByText( /сортировка массива/i )
+    expect( title ).toBeInTheDocument()
+  } )
+
+  test( 'correct routing to StackPage', async () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ [ '/stack' ] }>
+        <StackPage />
+      </MemoryRouter>
+    )
+    const title = getByText( /стек/i )
+    expect( title ).toBeInTheDocument()
+  } )
+
+  test( 'correct routing to QueuePage', async () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ [ '/queue' ] }>
+        <QueuePage />
+      </MemoryRouter>
+    )
+    const title = getByText( /очередь/i )
+    expect( title ).toBeInTheDocument()
+  } )
+
+  test( 'correct routing to ListPage', async () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ [ '/list' ] }>
+        <ListPage />
+      </MemoryRouter>
+    )
+    const title = getByText( /связный список/i )
+    expect( title ).toBeInTheDocument()
+  } )
+
 } )
