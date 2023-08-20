08fac072115d16d3be6e2e078e90bd3443098275
Aleksandr Petrov <petrov.aleksr@gmail.com>
2023-08-21 00:09:23 +0500

    refactor: Убрал дублирование кода из string-test.tsx

--- src/components/string/string-test.tsx
+++ src/components/string/string-test.tsx
@@ -2,28 +2,24 @@ import { getReversingStringSteps } from '../../utils/string-component-utils'
 
 describe( 'getReversingStringSteps', () => {
 
-	it( 'should correctly reverse a string with even number of characters', async () => {
-		const input = [ 'a', 'b', 'c', 'd' ]
+	async function testReversal ( input: string[], expectedOutput: string[] ) {
 		const result = await getReversingStringSteps( input )
-		expect( result[ result.length - 1 ] ).toEqual( [ 'd', 'c', 'b', 'a' ] )
+		expect( result[ result.length - 1 ] ).toEqual( expectedOutput )
+	}
+
+	it( 'should correctly reverse a string with even number of characters', () => {
+		testReversal( [ 'a', 'b', 'c', 'd' ], [ 'd', 'c', 'b', 'a' ] )
 	} )
 
-	it( 'should correctly reverse a string with odd number of characters', async () => {
-		const input = [ 'a', 'b', 'c' ]
-		const result = await getReversingStringSteps( input )
-		expect( result[ result.length - 1 ] ).toEqual( [ 'c', 'b', 'a' ] )
+	it( 'should correctly reverse a string with odd number of characters', () => {
+		testReversal( [ 'a', 'b', 'c', 'd', 'e' ], [ 'e', 'd', 'c', 'b', 'a' ] )
 	} )
 
-	it( 'should correctly reverse a string with one character', async () => {
-		const input = [ 'a' ]
-		const result = await getReversingStringSteps( input )
-		expect( result[ result.length - 1 ] ).toEqual( [ 'a' ] )
+	it( 'should correctly reverse a string with a single character', () => {
+		testReversal( [ 'a' ], [ 'a' ] )
 	} )
 
-	it( 'should correctly handle an empty string', async () => {
-		const input: string[] = []
-		const result = await getReversingStringSteps( input )
-		expect( result[ result.length - 1 ] ).toEqual( [] )
+	it( 'should correctly handle an empty string', () => {
+		testReversal( [], [] )
 	} )
-
 } )
